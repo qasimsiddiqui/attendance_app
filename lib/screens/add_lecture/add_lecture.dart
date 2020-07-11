aa901fa138b8e837c5073a78b93ed17316c74eaf
Qasim Siddiqui <m.qasim678@gmail.com>
2020-07-11 20:15:57 +0500

    renamed files

--- lib/screens/add_lecture/add_lecture.dart
+++ lib/screens/add_lecture/add_lecture.dart
@@ -1,6 +1,6 @@
 import 'package:attendance_app/models/user.dart';
-import 'package:attendance_app/screens/add_lecture/InstructorAddLecture.dart';
-import 'package:attendance_app/screens/add_lecture/StudentAddLecture.dart';
+import 'package:attendance_app/screens/add_lecture/instructor_addLecture.dart';
+import 'package:attendance_app/screens/add_lecture/student_addLecture.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
