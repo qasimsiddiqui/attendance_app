539ad9838afc619ac65ac7c7c392cc6f41c64f31
Qasim Siddiqui <m.qasim678@gmail.com>
2020-08-23 01:58:53 +0500

    Google SignIn method created

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,9 +1,11 @@
 import 'package:attendance_app/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:attendance_app/models/user.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   //create user object based on a firebase user
   UserUID _userFromFirebaseUser(FirebaseUser user) {
@@ -30,6 +32,29 @@ class AuthService {
   Stream<UserUID> get userChangeStream {
     return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
     //.map( (FirebaseUser user) => _userFromFirebaseUser(user) );
+  }
+
+  Future signInWithGoogle() async {
+    final GoogleSignInAccount googleSignInAccount =
+        await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return 'signInWithGoogle succeeded: $user';
   }
 
   //sign in anonymously
