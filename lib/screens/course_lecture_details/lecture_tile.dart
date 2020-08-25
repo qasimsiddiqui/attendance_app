8b47de2e62c2186c30ba00d8a62098e47b099f02
Qasim Siddiqui <m.qasim678@gmail.com>
2020-08-26 00:18:50 +0500

    percent_indicator displayed in lecture tiles

--- lib/screens/course_lecture_details/lecture_tile.dart
+++ lib/screens/course_lecture_details/lecture_tile.dart
@@ -3,17 +3,29 @@ import 'package:attendance_app/models/lecture.dart';
 import 'package:attendance_app/models/user.dart';
 import 'package:attendance_app/screens/lecture_details/lecture_details.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 import 'package:provider/provider.dart';
 
 class LectureTile extends StatelessWidget {
   final Lecture lecture;
 
   LectureTile({this.lecture});
+  String _percentageAttendance() {
+    if (double.parse(lecture.averageAttendance) == 100.00) {
+      return lecture.averageAttendance.substring(0, 3);
+    } else if (double.parse(lecture.averageAttendance) < 10.00) {
+      return lecture.averageAttendance.substring(0, 1);
+    } else {
+      return lecture.averageAttendance.substring(0, 2);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     User user = Provider.of<User>(context);
     Course course = Provider.of<Course>(context);
+    _percentageAttendance();
+    print(double.parse(lecture.averageAttendance));
     return Padding(
       padding: EdgeInsets.only(top: 0.0),
       child: Card(
@@ -25,7 +37,18 @@ class LectureTile extends StatelessWidget {
               child: Icon(Icons.dvr),
               radius: 25.0,
             ),
-            title: Text("${lecture.lectureName}"),
+            dense: false,
+            trailing: new CircularPercentIndicator(
+              radius: 50.0,
+              lineWidth: 5.0,
+              percent: double.parse(lecture.averageAttendance) / 100,
+              center: new Text(_percentageAttendance()),
+              progressColor: Colors.green,
+            ),
+            title: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: Text("${lecture.lectureName}"),
+            ),
             onTap: () {
               Navigator.push(
                   context,
